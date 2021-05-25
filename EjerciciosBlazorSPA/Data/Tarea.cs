8a258e4050ab8c2bdc0f3d86b42d33154462e480
Hadrián Villar <hadrianvillarcuadrado@gmail.com>
2021-05-25 15:21:05 +0200

    prueba con blazorise

--- EjerciciosBlazorSPA/Data/Tarea.cs
+++ EjerciciosBlazorSPA/Data/Tarea.cs
@@ -1,9 +1,4 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Threading.Tasks;
-
-namespace EjerciciosBlazorSPA.Data
+﻿namespace EjerciciosBlazorSPA.Data
 {
     public class Tarea
     {
