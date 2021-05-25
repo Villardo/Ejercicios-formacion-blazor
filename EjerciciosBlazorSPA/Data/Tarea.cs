fee687f34ce680659a89d978e77f6ba0b3a0a797
Hadrián Villar <hadrianvillarcuadrado@gmail.com>
2021-05-25 13:32:28 +0200

    Agregar archivos de proyecto.

---
+++ EjerciciosBlazorSPA/Data/Tarea.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace EjerciciosBlazorSPA.Data
+{
+    public class Tarea
+    {
+        public string Nombre { get; set; }
+        public bool Check { get; set; }
+    }
+}
