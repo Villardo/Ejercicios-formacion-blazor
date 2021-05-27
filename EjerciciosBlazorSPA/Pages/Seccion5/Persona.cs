c733a852078a2a0ec7b6cd10a326fbd949de720e
Hadrián Villar <hadrianvillarcuadrado@gmail.com>
2021-05-27 19:00:10 +0200

    seccion 5

---
+++ EjerciciosBlazorSPA/Pages/Seccion5/Persona.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using System.ComponentModel.DataAnnotations;
+
+
+namespace EjerciciosBlazorSPA.Pages.Seccion5
+{
+    public class Persona
+    {
+        [Required]
+        public string Nombre { get; set; }
+        [Range(0,Int32.MaxValue)]
+        public int Edad { get; set; }
+        [EmailAddress][Required]
+        public string Email { get; set; }
+        [Required]
+        public DateTime FechaNacimiento { get; set; }
+    }
+}
